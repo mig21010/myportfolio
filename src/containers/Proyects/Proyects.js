bfa1258e0ecc554e90e694378cfa26d271e11223
Miguel Es <mig2190@hotmail.com>
2018-04-22 00:49:02 -0500

    add proyects in routes

---
+++ src/containers/Proyects/Proyects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Container } from 'theme/grid';
+
+class Proyects extends Component {
+	render() {
+		return (
+			<Container>
+				<h1>My Proyects</h1>
+			</Container>
+		);
+	}
+}
+export default Proyects;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
-import Home from './containers/Home/Home';
-import App from './containers/App/App';
+import Home from 'containers/Home/Home';
+import App from 'containers/App/App';
+import Proyects from 'containers/Proyects/Proyects';
+
+
+Router.prototype.componentWillReceiveProps = function(nextProps) {
+  let components = [];
+  function grabComponents(element) {
+    // This only works for JSX routes, adjust accordingly for plain JS config
+    if (element.props && element.props.component) {
+      components.push(element.props.component);
+    }
+    if (element.props && element.props.children) {
+      React.Children.forEach(element.props.children, grabComponents);
+    }
+  }
+  grabComponents(nextProps.routes || nextProps.children);
+  components.forEach(React.createElement); // force patching
+};
 
 export default (
 	<Router history={hashHistory}>
 		<Route path="/" component={App}>
 			<IndexRoute component={Home}/>
+			<Route path="/proyects" component={Proyects}/>
 		</Route>
 	</Router>
 	);
