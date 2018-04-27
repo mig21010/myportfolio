aabde17dd29056eb4f528763f62863beca4020fd
Miguel Es <mig2190@hotmail.com>
2018-04-27 00:47:03 -0500

    add information about projects

--- src/containers/Proyects/Proyects.js
+++ src/containers/Proyects/Proyects.js
@@ -20,38 +20,83 @@ class Proyects extends Component {
 					</Index>
 					<h1>My Projects</h1>
 					<p>Ecommerce Application to sell products implemented with a cart system. <A href="">Checkout on GitHub.</A></p>
-					<Tag>#HTML #CSS #PHP #JavaScript #MySQL</Tag>
+					<Tag>#HTML#CSS#PHP#JavaScript#MySQL#HTML</Tag>
 				</Relative>
 				<Flex justify={'center'}>
 				<ImageButton>
-				<Image
-				 src={require('assets/past.png')} alt="paste"/>
+				<Image src={require('assets/past.png')} alt="paste"/>
 				</ImageButton>
 				</Flex>
-				<Relative>
+				<Relative marginBottom="50px">
 				<Index>
-
 				<h1>02</h1>
 					</Index>
-				<h2>Lorem ipsum dolor sit amet, consectetur <A href="">adipiscing elit.</A></h2>
+				<p>Gallery with PHP <A href='https://github.com/mig21010/Galeria'>Checkout on GitHub.</A></p>
+				<Tag>#PHP #MySQL#CSS#HTML</Tag>
 				</Relative>
 				<Flex justify={'center'}>
 				<ImageButton>
-				<img
-				 src={require('assets/imssproyecsmall.png')} alt="proyect"/>
+				<Image src={require('assets/galery.png')} alt="gallery"/>
 				</ImageButton>
 				</Flex>
-				<Relative>
+				<Relative marginBottom="50px">
 				<Index>
-
 				<h1>03</h1>
-					</Index>
-				<h2>Lorem ipsum dolor sit amet, consectetur <A href="">adipiscing elit.</A></h2>
+				</Index>
+				<p>Blog created with PHP and a database in MySQL <A href='https://github.com/mig21010/blog'>Checkout on GitHub.</A></p>
+				<Tag>#PHP #MySQL #CSS#HTML</Tag>
 				</Relative>
 				<Flex justify={'center'}>
 				<ImageButton>
-				<img
-				 src={require('assets/imssproyecsmall.png')} alt="proyect"/>
+				<Image src={require('assets/blog.png')} alt="blog"/>
+				</ImageButton>
+				</Flex>
+				<Relative marginBottom="50px">
+				<Index>
+				<h1>04</h1>
+				</Index>
+				<p>CRUD Application for a clinic to manage users, products, records and sales. <A href='https://github.com/mig21010/clinic'>Checkout on GitHub.</A></p>
+				<Tag>#PHP#MySQL#AdminLTE#AJAX#DataTables#JQuery#HTML#MVC#POO</Tag>
+				</Relative>
+				<Flex justify={'center'}>
+				<ImageButton>
+				<Image src={require('assets/clinica.png')} alt="clinic"/>
+				</ImageButton>
+				</Flex>
+				<Relative marginBottom="50px">
+				<Index>
+				<h1>05</h1>
+				</Index>
+				<p>Web Application for statistics and generate graphics. <A href='https://github.com/mig21010/Statisticsapp'>Checkout on GitHub.</A></p>
+				<Tag>#HTML#JQuery#Bootstrap#ChartJS</Tag>
+				</Relative>
+				<Flex justify={'center'}>
+				<ImageButton>
+				<Image src={require('assets/data.png')} alt="data"/>
+				</ImageButton>
+				</Flex>
+				<Relative marginBottom="50px">
+				<Index>
+				<h1>06</h1>
+				</Index>
+				<p>Portfolio Site developed with React Js <A href="">Checkout on GitHub.</A></p>
+				<Tag>#React#ES6#WebPack#StyledComponents</Tag>
+				</Relative>
+				<Flex justify={'center'}>
+				<ImageButton>
+				<Image src={require('assets/portfolio.png')} alt="portfolio"/>
+				</ImageButton>
+				</Flex>
+				<Relative marginBottom="50px">
+				<Index>
+				<h1>07</h1>
+				</Index>
+				<p>Final degree project in CodeIgniter about manage and genarate formats on PDF for IMSS <A href='https://github.com/mig21010/imss'>Checkout on GitHub.</A></p>
+				<Tag>#CodeIgniter#AJAX#JQuery#MySQL#HTML#MVC</Tag>
+				</Relative>
+				<Flex justify={'center'}>
+				<ImageButton>
+				<Image src={require('assets/imss.png')} alt="imss"/>
 				</ImageButton>
 				</Flex>
 			</Container>
